a3af38711f0e8e4272a9f0c543463e1a2007e223
itsWill <guilhermerpmansur@gmail.com>
2015-10-31 17:19:10 -0400

    finished events emitter started on reactor:

---
+++ lib/events_emitter.rb
@@ -0,0 +1,22 @@
+module EventsEmitter
+
+  # Returns an array with the callbacks listening on a specific event.
+  def listeners(event)
+    (@listeners ||= Hash.new{|h, k| h[k] = []})[event]
+  end
+
+  # Adds a new listener to the corresponding event.
+  def on(event, &block)
+    listeners(event) << block
+    self
+  end
+
+  # Will call each listener for an event in the order they where added
+  def emit(event, *args)
+    raise ArgumentError.new("Attempted to emit undefined event #{event}") if listeners(event).empty?
+
+    listeners(event).each do |blk|
+      blk.call(*args)
+    end
+  end
+end
---
+++ test/test_events_emitter.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/events_emitter'
+require 'minitest/autorun'
+
+class TestEventsEmitter < MiniTest::Unit::TestCase
+  include EventsEmitter
+
+  def test_on_and_emit
+    on(:test) do |a,b|
+      assert_equal a , b
+    end
+
+    emit(:test,"a","a")
+  end
+
+  def test_emit_on_undefined_event_raises_argument_error
+    assert_raises("ArgumentError"){ emit(:undefined) }
+  end
+end
